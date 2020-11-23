6899dedd9d4d301f71ce3b80de7be32a8be91e60
KintanaDesuK <buulong1999@gmail.com>
2020-11-23 17:41:06 +0700

    Update and Fix Auth

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,85 +3,73 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
+const cors = require('cors');
 
-const brandRoutes = require('./routes/brand-routes');  // đăng ký (midleware)
-const userRoutes = require('./routes/user-routes');
+const brandRoutes  = require('./routes/brand-routes'); 
+const userRoutes  = require('./routes/user-routes');
 const categoryRoutes = require('./routes/category-routes');
 const productRoutes = require('./routes/product-routes');
 const groupRoutes = require('./routes/group-routes');
+const sizeRoutes = require('./routes/size-routes');
+const orderRoutes = require('./routes/order-routes');
+const promotionRoutes = require('./routes/promotion-routes');
 const HttpError = require('./error-handle/http-error');
-const { get } = require('http');
-
-var cors = require('cors')
 
 const app = express();
-// app.use(bodyParser.json());
-
-// parse application/json
-app.use(bodyParser.json());
-
-app.use(cors());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(bodyParser.json());
+app.use(cors());
 
-// app.all('/', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next()
-//   });
 
 //Handling CORS Error
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers',
-        'X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Method', 'GET, POST, PATCH, DELETE');
-    next();
+    next(); 
 })
 
-// app.use(function (req, res, next) {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Headers", "*");
-//     next();
-//   });
-
 app.use('/api/product', productRoutes);
-//app.use('/api/bills',billRoutes);
 app.use('/api/brand', brandRoutes);
 app.use('/api/category', categoryRoutes);
-app.use('/api/user', userRoutes);
 app.use('/api/group', groupRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/size', sizeRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/promotion',promotionRoutes);
 
-app.get('/sync', (req, res) => {
+app.get('/sync', (req, res) =>{
     let models = require('./models');
     models.sequelize.sync()
-        .then(() => {
-            res.send('Database sync completed!')
-        });
+    .then(() =>{
+        res.send('Database sync completed!')
+    });
 });
 
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
-    throw (error);
+    throw(error);
 });
 
-app.use((error, req, res, next) => {        //Error handling
-    if (req.file) {
+app.use((error, req, res, next) => {       
+    if(req.file) {
         fs.unlink(req.file.path, (err) => {
             console.log(err);
         });
     }
-    if (res.headerSent) {
+    if(res.headerSent){
         return next(error);
     }
-    res.status(error.code || 500);  //code 500: something went wrong
-    res.json({ message: error.message || 'An unknown error occurred' });
+    res.status(error.code || 500);  
+    res.json({message: error.message || 'An unknown error occurred'});
 })
 
 
 //Start server
-app.listen(5000);
+app.listen(5000); 
 
 
 
